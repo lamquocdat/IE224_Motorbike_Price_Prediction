a3b73c5b7965500c3141abba53493594b28b946c
gameitthoi <lamdat3302@gmail.com>
2023-12-31 19:51:39 +0700

    add Debug status

--- webapp/run.py
+++ webapp/run.py
@@ -8,5 +8,5 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 if __name__ == '__main__':
     app = create_app()
     port = int(os.environ.get("PORT", 5000))
-
-    app.run(debug=True, port=port)
+    debug_status = os.environ.get("DEBUG", True)
+    app.run(debug=debug_status, port=port)
