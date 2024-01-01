f9596626085e6c036629ffbc4ff4ce98069a4347
gameitthoi <lamdat3302@gmail.com>
2024-01-01 09:08:48 +0700

    change run.py to app.py, build docker

--- webapp/app.py
+++ webapp/app.py
@@ -8,9 +8,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 if __name__ == '__main__':
     app = create_app()
     port = int(os.environ.get("PORT", 5000))
-    debug_status = os.environ.get("DEBUG", True)
-
-    if debug_status:
-        app.run(debug=True, port=port)
-    else:
-        app.run(host='0.0.0.0', port=port, debug=False)
+    debug_status = os.environ.get("DEBUG", "True").lower() == "true"
+    
+    app.run(host='0.0.0.0', port=port, debug=debug_status)
